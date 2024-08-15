44e969b1f04665c3bbc1062255c3504ff9b79a30
Jhonattan <jhonattanlino@outlook.com>
2024-08-14 22:07:13 -0300

    feat: create class with load data

--- src/script/data_transform.py
+++ src/script/data_transform.py
@@ -1,10 +1,27 @@
 """Data transform."""
+
 #%% Import libraries
 import pandas as pd
 from numpy import nan
 
-# %% Import data
-etf = pd.read_excel('src/data/etf.xlsx', index_col=0,
-                    parse_dates=True, skiprows=3)
+# %% Class Transform data
+class TransformData:
+    def __init__(self):
+        self.local = '/home/jhonattanln/hrp_etf/src/data/'
+        self.file = 'etf.xlsx'
+
+    def extract_data(self):
+        etf = pd.read_excel(self.local + self.file, index_col=0,
+                            parse_dates=True, skiprows=3)
+        global etf
+        return etf
+
+    def rename_columns(self):
+        et
+# %% Call class
+if __name__ == '__main__':
+    trasform = TransformData()
+    data = trasform.extract_data()
+    print(data)
 
 # %%
